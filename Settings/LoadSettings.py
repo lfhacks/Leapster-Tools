8b8a607079c422341041c53816e96c3c42c642e4
Dr. RNG <33909717+BLiNXthetimesweeperGOD@users.noreply.github.com>
2024-09-01 07:45:44 -0400

    Delete Settings directory
    Yet another restructure is about to happen

--- Settings/LoadSettings.py
+++
@@ -1,9 +0,0 @@
-def parseSettings(file, option):
-    with open(file, "r") as settings:
-        settingsList = settings.readlines()
-        for setting in settingsList:
-            if setting.startswith("?") == True:
-                continue
-            elif option in setting:
-                value = int(setting.split("=")[1].split("_")[0])
-                return value
